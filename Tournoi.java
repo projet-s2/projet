fad57578bc9083fa7df3f670f6aa2c69a5cdc0ae
paul604 <paulorhon@live.fr>
2016-04-18 14:36:44 +0200

    new

---
+++ Tournoi.java
@@ -0,0 +1,40 @@
+package tournoi;
+
+import liste.Liste;
+
+public class Tournoi{
+	
+	private Liste newJoueur;
+	private Liste ancienJoueur;
+	private Liste terrain;
+	private int nbrTerrains;
+	
+	public Tournoi(){
+		
+	}
+
+	public Liste getNewJoueur() {
+		return newJoueur;
+	}
+
+	public void ajouterJoueur(Joueur joueur) {
+		
+	}
+
+	public Liste getAncienJoueur() {
+		return ancienJoueur;
+	}
+
+	public Liste getTerrain() {
+		return terrain;
+	}
+	
+	public void initialiserTerrain(){
+		
+	}
+
+	public int getNbrTerrains() {
+		return nbrTerrains;
+	}
+	
+}
