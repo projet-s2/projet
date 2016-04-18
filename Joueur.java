b3c8eff0a519c3afec6ac9c916b42ff10b0b31e9
W4ldschr31n <nicolasrich@hotmail.fr>
2016-04-18 10:56:32 +0200

    Vrai joueur

--- Joueur.java
+++ Joueur.java
@@ -1,5 +1,27 @@
 package tournoi;
 
 public class Joueur {
+	private int id;
+	private String nom;
+	private String prenom;
+	private int age;
+	private char sexe;
+	private boolean nouveau;
+	private int score;
+	private boolean joue;
+	private String niveau;
+	
+	public Joueur(int id, String nom, String prenom, int age, char sexe,
+			boolean nouveau, String niveau){
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		this.sexe = sexe;
+		this.nouveau = nouveau;
+		this.score = 0;
+		this.joue = false;
+		this.niveau = niveau;
+	}
 
 }
