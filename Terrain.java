8a92dc9406655b239fecb07001936c4436df886c
W4ldschr31n <nicolasrich@hotmail.fr>
2016-04-18 14:02:15 +0200

    Nouveau terrain

---
+++ Terrain.java
@@ -0,0 +1,30 @@
+public class Terrain{
+  private int numero;
+  private Match match;
+
+  public Terrain(int numero, Match match){
+    this.numero=numero;
+    this.match.match;
+  }
+
+  public Terrain(int numero){
+    this.numero=numero;
+    this.match=null;
+  }
+
+  public void setNumero(int numero){
+    this.numero=numero;
+  }
+
+  public void setMatch(Match match){
+    this.match=match;
+  }
+
+  public int getNumero(){
+    return this.numero;
+  }
+
+  public Match getMatch(){
+    return this.match;
+  }
+}
