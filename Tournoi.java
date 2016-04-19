35487cf36b34d41a3aeef5e9aa8e0d59a054f460
W4ldschr31n <nicolasrich@hotmail.fr>
2016-04-19 16:14:31 +0200

    Méthodes ajoutées

--- Tournoi.java
+++ Tournoi.java
@@ -1,15 +1,15 @@
 package tournoi;
-
+import java.math.*;
 import liste.Liste;
 
 public class Tournoi{
-
+	
 	private Liste nouveauxJoueurs;
 	private Liste anciensJoueurs;
 	private Liste terrains;
 	private Liste paires;
 	private int nbrTerrains;
-
+	
 	public Tournoi(int nbrTerrains){
 		this.nouveauxJoueurs= new Liste();
 		this.anciensJoueurs= new Liste();
@@ -18,28 +18,19 @@ public class Tournoi{
 		this.nbrTerrains= nbrTerrains;
 	}
 
-	public Liste getNewJoueur() {
+	public Liste getNouveauxJoueurs() {
 		return this.nouveauxJoueurs;
 	}
 
-	public void ajouterJoueur(Joueur joueur) {
-		if (joueur.getNouveau()==false){
-			anciensJoueurs.add(joueur);
-		}
-		else {
-			nouveauxJoueurs.add(joueur);
-		}
-	}
-
-	public Liste getAncienJoueur() {
+	public Liste getAnciensJoueurs() {
 		return this.anciensJoueurs;
 	}
 
-	public Liste getTerrain() {
+	public Liste getTerrains() {
 		return this.terrains;
 	}
-
-	public void initialiserTerrain(){
+	
+	public void initialiserTerrains(){
 		for (int i=0; i<this.nbrTerrains; i++){
 			this.terrains.add(new Terrain(i+1));
 		}
@@ -48,7 +39,7 @@ public class Tournoi{
 	public int getNbrTerrains() {
 		return this.nbrTerrains;
 	}
-
+	
 	public void ajouterjoueur(Joueur joueur){
 		if (joueur.getNouveau()){
 			this.nouveauxJoueurs.add(joueur);}
@@ -56,31 +47,77 @@ public class Tournoi{
 			this.anciensJoueurs.add(joueur);
 		}
 	}
-
+	
 	public void demarrerTour(){
 		this.creerPaires();
 		this.attribuerMatchs();
+		//On remet tous les joueurs en attente d'une paire
+		for (int i=0; i<this.anciensJoueurs.size(); i++){
+			((Joueur)this.anciensJoueurs.get(i)).setJoue(false);
+		}
+		for (int i=0; i<this.nouveauxJoueurs.size(); i++){
+			((Joueur)this.nouveauxJoueurs.get(i)).setJoue(false);
+		}
 	}
-
-	public void creerPaires(){
+	
+	private void creerPaires(){
 		//On parcourt les deux listes de joueurs et on crée les paires en conséquence
-
+		int tailleMin;
+		Joueur joueur;
+		if ( this.anciensJoueurs.size()>=this.nouveauxJoueurs.size() ){
+			tailleMin=this.nouveauxJoueurs.size();
+			for (int j=0; j<this.anciensJoueurs.size(); j++)
+			{
+				joueur = ((Joueur)this.anciensJoueurs.get(j));
+				for (int i=0; i<tailleMin; i++){
+					if (joueur.estCompatible(((Joueur)this.nouveauxJoueurs.get(i))){
+						this.paires.add(new Paire(joueur,((Joueur)this.nouveauxJoueurs.get(i)),i,i));
+						joueur.setJoue(true);
+						((Joueur)this.nouveauxJoueurs.get(i)).setJoue(true);
+						break;
+					}
+				}
+			}
+		}
+		else{
+			tailleMin=this.anciensJoueurs.size();
+			for (int j=0; j<this.nouveauxJoueurs.size(); j++)
+			{
+				joueur = ((Joueur)this.nouveauxJoueurs.get(j));
+				for (int i=0; i<tailleMin; i++){
+					if (joueur.estCompatible(((Joueur)this.anciensJoueurs.get(i))){
+						this.paires.add(new Paire(joueur,((Joueur)this.anciensJoueurs.get(i)),i,i));
+						joueur.setJoue(true);
+						((Joueur)this.anciensJoueurs.get(i)).setJoue(true);
+						break;
+					}
+				}
+			}
+		}
 	}
-
-	public void attribuerMatchs(){
+	
+	private void attribuerMatchs(){
 		Paire paire1,paire2;
 		//On parcourt les terrains et on leur attribue des matchs que l'on crée à partir des paires
 		for (int i=0; i<this.nbrTerrains; i++){
-			//paire 1 =???
-			//paire 2 =???
+			paire1=((Paire)this.paires.get(2*i));
+			paire2=((Paire)this.paires.get(2*i+1));
 			((Terrain)this.terrains.get(i)).setMatch(new Match(i+1, paire1, paire2));
 		}
 	}
-
+	
 	public void finirTour(){
 		//On demande le score des équipes pour chaque terrain
-		//On détermine les vainqueurs de chaque match
-		//On modifie le score des joueurs en conséquence
-	}
+		for (int i=0; i<this.nbrTerrains; i++){
+			((Terrain)this.terrains.get(i)).getMatch().getPaire1().setScore(0);
+			((Terrain)this.terrains.get(i)).getMatch().getPaire2().setScore(0);
+			//On détermine les vainqueurs de chaque match
+			((Terrain)this.terrains.get(i)).getMatch().setVainqueur(((Terrain)this.terrains.get(i)).getMatch().determinerVainqueur());
+			//On modifie le score des joueurs en conséquence
+			((Terrain)this.terrains.get(i)).getMatch().modifierScores();
 
+
+			}
+	}
+	
 }
