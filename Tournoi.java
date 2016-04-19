84ae3180417787832771f8abade17b083849afde
Youlounn <yohann.rialet28@gmail.com>
2016-04-19 15:14:40 +0200

    Added files via upload

--- Tournoi.java
+++ Tournoi.java
@@ -1,15 +1,15 @@
 package tournoi;
-import java.math.*;
+
 import liste.Liste;
 
 public class Tournoi{
-	
+
 	private Liste nouveauxJoueurs;
 	private Liste anciensJoueurs;
 	private Liste terrains;
 	private Liste paires;
 	private int nbrTerrains;
-	
+
 	public Tournoi(int nbrTerrains){
 		this.nouveauxJoueurs= new Liste();
 		this.anciensJoueurs= new Liste();
@@ -18,19 +18,28 @@ public class Tournoi{
 		this.nbrTerrains= nbrTerrains;
 	}
 
-	public Liste getNouveauxJoueurs() {
+	public Liste getNewJoueur() {
 		return this.nouveauxJoueurs;
 	}
 
-	public Liste getAnciensJoueurs() {
+	public void ajouterJoueur(Joueur joueur) {
+		if (joueur.getNouveau()==false){
+			anciensJoueurs.add(joueur);
+		}
+		else {
+			nouveauxJoueurs.add(joueur);
+		}
+	}
+
+	public Liste getAncienJoueur() {
 		return this.anciensJoueurs;
 	}
 
-	public Liste getTerrains() {
+	public Liste getTerrain() {
 		return this.terrains;
 	}
-	
-	public void initialiserTerrains(){
+
+	public void initialiserTerrain(){
 		for (int i=0; i<this.nbrTerrains; i++){
 			this.terrains.add(new Terrain(i+1));
 		}
@@ -39,7 +48,7 @@ public class Tournoi{
 	public int getNbrTerrains() {
 		return this.nbrTerrains;
 	}
-	
+
 	public void ajouterjoueur(Joueur joueur){
 		if (joueur.getNouveau()){
 			this.nouveauxJoueurs.add(joueur);}
@@ -47,43 +56,31 @@ public class Tournoi{
 			this.anciensJoueurs.add(joueur);
 		}
 	}
-	
+
 	public void demarrerTour(){
 		this.creerPaires();
 		this.attribuerMatchs();
 	}
-	
-	private void creerPaires(){
+
+	public void creerPaires(){
 		//On parcourt les deux listes de joueurs et on crée les paires en conséquence
-		for (int i=0; i<Math.min(this.anciensJoueurs.size(),this.nouveauxJoueurs.size()); i++){
-				this.paires.add(new Paire(((Joueur)this.anciensJoueurs.get(i)),((Joueur)this.nouveauxJoueurs.get(i)),i,i));
-				((Joueur)this.anciensJoueurs.get(i)).setJoue(true);
-				((Joueur)this.nouveauxJoueurs.get(i)).setJoue(true);
-		}
+
 	}
-	
-	private void attribuerMatchs(){
+
+	public void attribuerMatchs(){
 		Paire paire1,paire2;
 		//On parcourt les terrains et on leur attribue des matchs que l'on crée à partir des paires
 		for (int i=0; i<this.nbrTerrains; i++){
-			paire1=((Paire)this.paires.get(2*i));
-			paire2=((Paire)this.paires.get(2*i+1));
+			//paire 1 =???
+			//paire 2 =???
 			((Terrain)this.terrains.get(i)).setMatch(new Match(i+1, paire1, paire2));
 		}
 	}
-	
+
 	public void finirTour(){
 		//On demande le score des équipes pour chaque terrain
-		for (int i=0; i<this.nbrTerrains; i++){
-			((Terrain)this.terrains.get(i)).getMatch().getPaire1().setScore(0);
-			((Terrain)this.terrains.get(i)).getMatch().getPaire2().setScore(0);
-			//On détermine les vainqueurs de chaque match
-			((Terrain)this.terrains.get(i)).getMatch().setVainqueur(((Terrain)this.terrains.get(i)).getMatch().determinerVainqueur());
-			//On modifie le score des joueurs en conséquence
-			((Terrain)this.terrains.get(i)).getMatch().modifierScores();
-
-
-			}
+		//On détermine les vainqueurs de chaque match
+		//On modifie le score des joueurs en conséquence
 	}
-	
+
 }
