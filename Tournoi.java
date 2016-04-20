d7ec30e055d6bafa602c7dbe475fa1b62e572d31
Youlounn <yohann.rialet28@gmail.com>
2016-04-20 10:54:12 +0200

    Javadoc

--- Tournoi.java
+++ Tournoi.java
@@ -3,20 +3,24 @@ import java.math.*;
 import liste.Liste;
 
 /**Tournoi est la classe représentant un Tournoi.
- * 
- * @author Yanis OUAKRIM, RICHARD Nicolas, ORHON Paul, RIALET Yohann, NIVELAIS Quentin
- * 
+ *
+ * @author OUAKRIM Yanis, RICHARD Nicolas, ORHON Paul, RIALET Yohann, NIVELAIS Quentin
+ *
  * @version 0.1
  */
 
 public class Tournoi{
-	
+
 	private Liste nouveauxJoueurs;
 	private Liste anciensJoueurs;
 	private Liste terrains;
 	private Liste paires;
 	private int nbrTerrains;
-	
+/** Constructeur de la classe tournoi
+	*
+	* @param nbrTerrains
+	*
+	*/
 	public Tournoi(int nbrTerrains){
 		this.nouveauxJoueurs= new Liste();
 		this.anciensJoueurs= new Liste();
@@ -25,28 +29,58 @@ public class Tournoi{
 		this.nbrTerrains= nbrTerrains;
 	}
 
+/** Retourne la liste de nouveaux joueurs
+	*
+	* @return Une liste de Tournoi, qui correspond à celle de tous les nouveaux joueurs
+	*
+	*/
 	public Liste getNouveauxJoueurs() {
 		return this.nouveauxJoueurs;
 	}
 
+	/** Retourne la liste des anciens joueurs
+		*
+		* @return Une liste de Tournoi, qui correspond à celle de tous les anciens joueurs
+		*
+		*/
 	public Liste getAnciensJoueurs() {
 		return this.anciensJoueurs;
 	}
 
+	/** Retourne la liste des terrains
+		*
+		* @return Une liste de Tournoi, qui correspond à celle de tous les terrains
+		*
+		*/
 	public Liste getTerrains() {
 		return this.terrains;
 	}
-	
+
+	/** Initialise tous les terrains
+		*
+		*
+		*
+		*/
 	public void initialiserTerrains(){
 		for (int i=0; i<this.nbrTerrains; i++){
 			this.terrains.add(new Terrain(i+1));
 		}
 	}
 
+	/** Retourne le nombre de terrains
+		*
+		* @return Un entier de Tournoi, correspondant au nombre de terrains
+		*
+		*/
 	public int getNbrTerrains() {
 		return this.nbrTerrains;
 	}
-	
+
+	/** Ajoute un joueur à la liste des joueurs d'un tournoi
+		*
+		* @param joueur
+		*
+		*/
 	public void ajouterjoueur(Joueur joueur){
 		if (joueur.getNouveau()){
 			this.nouveauxJoueurs.add(joueur);}
@@ -54,7 +88,12 @@ public class Tournoi{
 			this.anciensJoueurs.add(joueur);
 		}
 	}
-	
+
+	/** Appelée pour démarrer un tour
+		*
+		*
+		*
+		*/
 	public void demarrerTour(){
 		this.creerPaires();
 		this.attribuerMatchs();
@@ -66,7 +105,12 @@ public class Tournoi{
 			((Joueur)this.nouveauxJoueurs.get(i)).setJoue(false);
 		}
 	}
-	
+
+	/** Appelée pour créer la liste des paires d'un tournoi
+		*
+		*
+		*
+		*/
 	private void creerPaires(){
 		//On parcourt les deux listes de joueurs et on crée les paires en conséquence
 		int tailleMin;
@@ -77,7 +121,7 @@ public class Tournoi{
 			{
 				joueur = ((Joueur)this.anciensJoueurs.get(j));
 				for (int i=0; i<tailleMin; i++){
-					if (joueur.estCompatible(((Joueur)this.nouveauxJoueurs.get(i)))){
+					if (joueur.estCompatibleAvec(((Joueur)this.nouveauxJoueurs.get(i)))){
 						this.paires.add(new Paire(joueur,((Joueur)this.nouveauxJoueurs.get(i)),i,i));
 						joueur.setJoue(true);
 						((Joueur)this.nouveauxJoueurs.get(i)).setJoue(true);
@@ -92,7 +136,7 @@ public class Tournoi{
 			{
 				joueur = ((Joueur)this.nouveauxJoueurs.get(j));
 				for (int i=0; i<tailleMin; i++){
-					if (joueur.estCompatible(((Joueur)this.anciensJoueurs.get(i)))){
+					if (joueur.estCompatibleAvec(((Joueur)this.anciensJoueurs.get(i)))){
 						this.paires.add(new Paire(joueur,((Joueur)this.anciensJoueurs.get(i)),i,i));
 						joueur.setJoue(true);
 						((Joueur)this.anciensJoueurs.get(i)).setJoue(true);
@@ -102,7 +146,12 @@ public class Tournoi{
 			}
 		}
 	}
-	
+
+	/** Attribue un match à deux paires
+		*
+		*
+		*
+		*/
 	private void attribuerMatchs(){
 		Paire paire1,paire2;
 		//On parcourt les terrains et on leur attribue des matchs que l'on crée à partir des paires
@@ -112,7 +161,12 @@ public class Tournoi{
 			((Terrain)this.terrains.get(i)).setMatch(new Match(i+1, paire1, paire2));
 		}
 	}
-	
+
+	/** Appelée pour finir un tour du tournoi
+		*
+		*
+		*
+		*/
 	public void finirTour(){
 		//On demande le score des équipes pour chaque terrain
 		for (int i=0; i<this.nbrTerrains; i++){
@@ -126,5 +180,5 @@ public class Tournoi{
 
 			}
 	}
-	
+
 }
