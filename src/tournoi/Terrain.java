9447c85a768203b57fb192d73d397fb45686410d
Youlounn <yohann.rialet28@gmail.com>
2016-04-20 14:48:48 +0200

    Javadoc

--- src/tournoi/Terrain.java
+++ src/tournoi/Terrain.java
@@ -1,31 +1,69 @@
 package tournoi;
 
+/**Terrain est la classe représentant un terrain du tournoi.
+ *
+ * @author OUAKRIM Yanis, RICHARD Nicolas, ORHON Paul, RIALET Yohann, NIVELAIS Quentin
+ *
+ * @version 0.1
+ */
 public class Terrain{
   private int numero;
   private Match match;
 
+  /**Constructeur de la classe Terrain
+   *
+   * @param numero le numero du terrain
+   * @param match le match associé au terrain
+   *
+   * @version 0.1
+   */
   public Terrain(int numero, Match match){
     this.numero=numero;
     this.match=match;
   }
 
+  /**Redéfinition du constructeur d'un terrain
+   *
+   * @param numero le numero d'un terrain
+   *
+   */
   public Terrain(int numero){
     this.numero=numero;
     this.match=null;
   }
 
+  /**Redéfinit le numéro d'un terrain
+   *
+   * @param numero le nouveau numéro d'un terrain
+   *
+   */
   public void setNumero(int numero){
     this.numero=numero;
   }
 
+  /**Redéfinit le match d'un terrain
+   *
+   * @param match le nouveau match d'un terrain
+   *
+   */
   public void setMatch(Match match){
     this.match=match;
   }
 
+  /**Retourne le numéro d'un terrain
+   *
+   * @return numero le numéro d'un terrain
+   *
+   */
   public int getNumero(){
     return this.numero;
   }
 
+  /**Retourne le match d'un terrain
+   *
+   * @return match le match d'un terrain
+   *
+   */
   public Match getMatch(){
     return this.match;
   }
