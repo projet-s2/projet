dc201edde1ba667c6ac86d334443ba519ca97fd3
W4ldschr31n <nicolasrich@hotmail.fr>
2016-06-10 08:57:54 +0200

    bug

--- src/controleur/AjouterJoueurControlleur.java
+++ src/controleur/AjouterJoueurControlleur.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 
 import vue.FenetreAjoutJoueur;
 
+import javax.swing.*;
+
 public class AjouterJoueurControlleur implements ActionListener {
 	
 	private FenetreAjoutJoueur vue;
@@ -17,18 +19,28 @@ public class AjouterJoueurControlleur implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (verifier()){
 			vue.ajouterJoueur();
+			JOptionPane.showMessageDialog(vue,"Joueur ajouté !");
 		}
 
 	}
 
 	
 	public boolean verifier(){
-		//reg = "([a-zA-Z]*\s*[-']*)*";
-		if ( vue.getNom().getText().matches("([a-zA-Z]*\s*[-']*)*") && vue.getPrenom().getText().matches("([a-zA-Z]*\s*[-']*)*") ){
-			return true;
-			
+		if (vue.getNom().getText().equals("") || vue.getPrenom().getText().equals("")){
+			JOptionPane.showMessageDialog(vue,"Merci d'indiquer un nom et un prenom");
+			return false;
 		}
-		
-		return false;
+		//Le nom et le prénom sont composés de lettres non accentuées et éventuellement de tirets, espaces ou apostrophes
+		String reg = "([[a-zA-Z][-][ ']]*)*";
+		if ( !vue.getNom().getText().matches(reg) || !vue.getPrenom().getText().matches(reg) ){
+			JOptionPane.showMessageDialog(vue,"Le nom et le prénom doivent être composé de lettres non accentuées\n avec éventuellement des tirets -, espaces ou apostrophes '");
+			return false;
+		}
+		//Verification sur l'age
+		if ((int)vue.getAge().getValue()<=0){
+			JOptionPane.showMessageDialog(vue,"L'age doit etre un entier positif");
+			return false;
+		}
+	return true;
 	}
 }
