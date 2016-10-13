b86f9589b1862e1b720781a1a2e9cd93dee15c3f
Cyril DERNONCOURT <E155545W@U-INF-J-C001-21.iut-nantes.univ-nantes.prive>
2016-10-13 11:05:25 +0200

    Controleur pour supprimer un joueur

--- src/controleur/SupprimerJoueurBoutonControlleur.java
+++ src/controleur/SupprimerJoueurBoutonControlleur.java
@@ -1,7 +1,26 @@
-package src.controleur;
+package controleur;
 
-/**
- * Created by E155545W on 12/10/16.
- */
-public class SupprimerJoueurBoutonControlleur {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import vue.FenetreModifierJoueur;
+
+
+public class SupprimerJoueurBoutonControlleur implements ActionListener
+{
+    private FenetreModifierJoueur fmj;
+    private int id;
+
+    public SupprimerJoueurBoutonControlleur(FenetreModifierJoueur fmj, int id)
+    {
+        this.fmj = fmj;
+        this.id = id;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        fmj.supprimerJoueur(this.id);
+    }
 }
