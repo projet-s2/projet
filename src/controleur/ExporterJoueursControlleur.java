fb11aee71a90046289f93d81ad80e6c63e122dce
Martineau Lucas <lucasmartineau@yahoo.fr>
2016-10-15 18:11:50 +0200

    Amélioration + javadoc + commentaires Import/export

--- src/controleur/ExporterJoueursControlleur.java
+++ src/controleur/ExporterJoueursControlleur.java
@@ -2,26 +2,27 @@ package controleur;
 
 import tournoi.Joueur;
 import tournoi.Tournoi;
-import vue.FenetrePrincipale;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 
-/**
- * Created by E155301F on 12/10/16.
+/** La classe ExporterJoueursControlleur permet d'exporter les anciens ainsi que les nouveaux
+ *  joueurs contenus dans la classe tournoi dans un fichier CSV
+ * @author DERNONCOURT Cyril , DROUARD Antoine, LE BERT Lea, MARTINEAU Lucas
  */
 public class ExporterJoueursControlleur implements ActionListener {
 
-    private Tournoi tournoi;
-    private FenetrePrincipale vue;
+    private final Tournoi tournoi;
 
-    public ExporterJoueursControlleur(Tournoi tournoi, FenetrePrincipale vue) {
+    /** Constructeur de la classe ExporterJoueursControlleur
+     *
+     * @param tournoi le tournoi où trouver les joueurs à exporter
+     */
+    public ExporterJoueursControlleur(Tournoi tournoi) {
         this.tournoi = tournoi;
-        this.vue = vue;
     }
 
     @Override
@@ -33,48 +34,45 @@ public class ExporterJoueursControlleur implements ActionListener {
         dial.setVisible(true);
         fr.setVisible(false);
         try {
-            /* On crée un BufferedWriter (FileWriter avec la possibilité de créer une nouvelle ligne)
-                qui va créer un fichier du nom qu'à choisi l'utilisateur et écrire dans celui ci
-            */
-            BufferedWriter fichier = new BufferedWriter(new FileWriter (dial.getDirectory().concat(dial.getFile()) ));
-            for (Joueur j : tournoi.getAnciensJoueurs()) {
-                fichier.write(decouperJoueur(j));
-                fichier.newLine();
-            }
+            if (dial.getDirectory() != null && dial.getFile()!= null) {// Si l'utilisateur a bien spécifié un fichier où écrire
+                // On crée un BufferedWriter (FileWriter avec la possibilité de créer une nouvelle ligne)
+                // qui va créer un fichier du nom qu'à choisi l'utilisateur et écrire dans celui ci
+                BufferedWriter fichier = new BufferedWriter(new FileWriter (dial.getDirectory().concat(dial.getFile()) ));
 
-            for (Joueur j : tournoi.getNouveauxJoueurs()) {
-                fichier.write(decouperJoueur(j));
-                fichier.newLine();
+                //On parcourt tous les anciensJoueurs de tournoi, on les découpe et on les écrit dans le fichier
+                for (Joueur j : tournoi.getAnciensJoueurs()) {
+                    fichier.write(decouperJoueur(j));
+                    fichier.newLine();
+                }
+                //On parcourt tous les NoueauxJoueurs de tournoi, on les découpe et on les écrit dans le fichier
+                for (Joueur j : tournoi.getNouveauxJoueurs()) {
+                    fichier.write(decouperJoueur(j));
+                    fichier.newLine();
+                }
+                fichier.close();
             }
-            fichier.close();
-
-        } catch (IOException e1) {
+        } catch (Exception e1) {
             e1.printStackTrace();
-        } catch (NullPointerException e2) {
-            e2.printStackTrace();
-        } catch (Exception e3) {
-            e3.printStackTrace();
         }
     }
+
     /** Découpe un joueur pour obtenir une chaine de caractère correspondant à une ligne CSV
      * @param joueur le joueur à découper
      * @return La chaine de caractère correspondant à une ligne CSV représentant le joueur découpé
      */
     private String decouperJoueur(Joueur joueur) {
-        /* [0] : id / [1] : nom / [2] : prenom / [3] : age / [4] : sexe (0 : homme / 1 : femme)
-               [5] : nouveau (0 : ancien / 1 : nouveau) / [6] : niveau  (0 : débutant / 1 : Intérmédiaire / 2 : confirmé)
-               [7] : peutJouer
-            */
+        // Ordre du fichier CSV :
+        // [0] : id / [1] : nom / [2] : prenom / [3] : age / [4] : sexe (0 : homme / 1 : femme)
+        // [5] : nouveau (0 : ancien / 1 : nouveau) / [6] : niveau  (0 : débutant / 1 : Intérmédiaire / 2 : confirmé)
+        // [7] : peutJouer
 
-        StringBuilder chaine = new StringBuilder();
-        chaine.append(Integer.toString(joueur.getId()) + ",");
-        chaine.append(joueur.getNom() + ",");
-        chaine.append(joueur.getPrenom() + ",");
-        chaine.append(Integer.toString(joueur.getAge()) + ",");
-        chaine.append(joueur.getSexe() ? "1," : "0,");
-        chaine.append(joueur.getNouveau() ? "1," : "0,");
-        chaine.append(Integer.toString(joueur.getNiveau()) + ",");
-        chaine.append(joueur.peutJouer() ? "1" : "0");
-        return chaine.toString();
+        return Integer.toString(joueur.getId()) + "," +
+                joueur.getNom() + "," +
+                joueur.getPrenom() + "," +
+                Integer.toString(joueur.getAge()) + "," +
+                (joueur.getSexe() ? "1," : "0,") +
+                (joueur.getNouveau() ? "1," : "0,") +
+                Integer.toString(joueur.getNiveau()) + "," +
+                (joueur.peutJouer() ? "1" : "0");
     }
 }
