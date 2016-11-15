0d97d266baae889bfce841c7f47fbd22e1a76536
Cyril Dernoncourt <cyril.dernoncourt@etu.univ-nantes.fr>
2016-11-15 11:02:59 +0100

    Affichage du classement avec le filtre pour définir l'ancienneté

--- src/vue/FenetreClassement.java
+++ src/vue/FenetreClassement.java
@@ -6,6 +6,7 @@ import tournoi.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class FenetreClassement extends JFrame
 {
@@ -14,7 +15,6 @@ public class FenetreClassement extends JFrame
     private DefaultTableModel listeJoueursModele;
     private JTable listeJoueurs;
     private JComboBox categorie;
-    String[] joueurs;
 
 
     /**
@@ -28,19 +28,47 @@ public class FenetreClassement extends JFrame
 
         JPanel joueurs = new JPanel();
         joueurs.setLayout(new GridLayout(1, 2));
+
         String  title[] = {"Nom", "Prénom", "Score","Ancienneté"};
-        listeJoueursModele = new DefaultTableModel(title,0);
+
+        listeJoueursModele = new DefaultTableModel(title, 0)
+        {
+            //bien redefinir les types des colones pour que l'autosort marche
+            public Class getColumnClass(int column)
+            {
+                switch (column)
+                {
+                    case 0:
+                        return String.class;
+                    case 1:
+                        return String.class;
+                    case 2:
+                        return Integer.class;
+                    case 3:
+                        return String.class;
+                    default:
+                        return String.class;
+                }
+            }
+        };
+
         listeJoueurs = new JTable(listeJoueursModele);
         listeJoueurs.setAutoCreateRowSorter(true);
         panJoueurs = new JScrollPane(listeJoueurs);
+
         joueurs.add(panJoueurs);
+
+
 
         JPanel a = new JPanel();
         categorie = new JComboBox(new String[]{"Tous", "Nouveaux", "Anciens"});
         categorie.setSelectedIndex(0);
-        //categorie.addActionListener(new AfficherClassementControleur(this, categorie));
+        categorie.addActionListener(new AfficherClassementControleur(this, categorie));
         a.add(categorie);
         joueurs.add(a);
+
+        afficherJoueur("Tous");
+
 
         this.setContentPane(joueurs);
         this.pack();
@@ -51,4 +79,57 @@ public class FenetreClassement extends JFrame
         this.setSize(tailleX,tailleY);
         this.setResizable(false);
     }
+
+    public void afficherJoueur(String s)
+    {
+        listeJoueursModele.setRowCount(0);
+
+
+        if(s.equals("Tous"))
+        {
+            ArrayList<Joueur> listeJoueurNouveau = tournoi.getNouveauxJoueurs();
+            ArrayList<Joueur> listeJoueurAncien = tournoi.getAnciensJoueurs();
+            for (Joueur j : listeJoueurNouveau)
+            {
+                ajouterJoueurTable(j);
+            }
+            for (Joueur j : listeJoueurAncien)
+            {
+                ajouterJoueurTable(j);
+            }
+        }
+
+        if(s.equals("Nouveaux"))
+        {
+            ArrayList<Joueur> listeJoueurNouveau = tournoi.getNouveauxJoueurs();
+            for (Joueur j : listeJoueurNouveau)
+            {
+                ajouterJoueurTable(j);
+            }
+        }
+
+        if(s.equals("Anciens"))
+        {
+            ArrayList<Joueur> listeJoueurAncien = tournoi.getAnciensJoueurs();
+            for (Joueur j : listeJoueurAncien)
+            {
+                ajouterJoueurTable(j);
+            }
+        }
+    }
+
+    public void ajouterJoueurTable(Joueur j)
+    {
+        String ancien = "Ancien";
+        if(j.getNouveau())
+        {
+            ancien = "Nouveau";
+        }
+        Object[] tJ = {j.getNom(), j.getPrenom(), j.getScore(), ancien};
+        this.listeJoueursModele.addRow(tJ);
+    }
+
+
+
+
 }
